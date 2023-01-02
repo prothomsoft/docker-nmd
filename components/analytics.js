bd994e7190dc28282334e8633b1c549c8d915fbb
Tomasz Prokop <tomasz@99foto.pl>
2023-01-02 14:52:38 +0100

    new tag

--- components/analytics.js
+++ components/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-    ReactGA.initialize("G-WMWEG5RRRG");
+    ReactGA.initialize("G-0MHSG716QK");
 };
 
 export const logPageView = () => {
