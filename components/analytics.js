641d1d37b883f58f8ae7bd5bf0b0a418f91be2f0
Tomasz Prokop <tomasz@99foto.pl>
2023-01-02 14:16:35 +0100

    logEvent latest

--- components/analytics.js
+++ components/analytics.js
@@ -5,18 +5,5 @@ export const initGA = () => {
 };
 
 export const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname);
-};
-
-export const logEvent = (category = "", action = "") => {
-    if (category && action) {
-        ReactGA.event({ category, action });
-    }
-};
-
-export const logException = (description = "", fatal = false) => {
-    if (description) {
-        ReactGA.exception({ description, fatal });
-    }
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 };
