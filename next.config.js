b5703838be2aae8252020911b1de03bfd0c4453c
Tomasz Prokop <tomasz@99foto.pl>
2023-12-24 09:16:38 +0100

    latest changes after upgrade

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
   swcMinify: true,
   output: 'standalone',
   images: {
-    domains: ['sk99foto.s3.eu-west-3.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sk99foto.s3.eu-west-3.amazonaws.com',
+        pathname: '**',
+      },
+    ],
     deviceSizes: [640, 750, 828, 1080, 1140]
   },
   env: {
