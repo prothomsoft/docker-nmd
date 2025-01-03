4820541f0ed4359497891b9b542e90f1b660d529
Tomasz Prokop <tprokop@csod.com>
2025-01-03 16:27:58 +0100

    latest

--- pages/strefa-klienta.js
+++ pages/strefa-klienta.js
@@ -5,7 +5,7 @@ import ClientList from "../components/clientList";
 
 const BlogListComponent = ({ posts }) => {
 
-  const pageImage = process.env.staticS3ImagesPath + "sala_weselna_biala_wilczyca_kocmyrzow.webp";
+  const pageImage = process.env.staticS3ImagesPath + "sala_weselna_willa_orchidea_waksmund.webp";
 
   return (
     <LayoutSitePage
@@ -13,9 +13,9 @@ const BlogListComponent = ({ posts }) => {
       description="Strefa klienta to miejsce, w którym możesz oglądać zdjęcia i dzielić się nimi ze znajomymi i rodziną. Serdecznie zapraszam !"
       keywords="Strefa klienta, moje zdjęcia, dobre zdjęcia"
       url="https://99foto.pl/strefa-klienta/"
-      leadNames="MONIKA i MARTIN"
-      leadTitle="WESELE MIĘDZYNARODOWE i POLSKIE TRADYCJE WESELNE"
-      leadUrl="/wesele-miedzynarodowe-proszowice-i-polskie-tradycje-weselne"
+      leadNames="WERONIKA i MARCIN"
+      leadTitle="SALA WESELNA WILLA ORCHIDEA WAKSMUND - ŚLUB NA PODHALU"
+      leadUrl="/sala-weselna-willa-orchidea-waksmund-slub-w-pieninach"
       leadImage={pageImage}>
       <Container sx={{ pt:8, pb: 5 }}>
         <Box sx={{pt:13, display: { xs: "none", lg: "block", xl: "block" }}}></Box>
